673f6eb346e3bf07c7f79bc1801c2cab57178e32
Nicolas Teixeira <nicolasteixeira3856@outlook.com>
2022-01-11 20:02:58 -0300

    move api details from domain entity to data model

---
+++ lib/data/models/remote_account_model.dart
@@ -0,0 +1,13 @@
+import 'package:for_dev/domain/entities/account_entity.dart';
+
+class RemoteAccountModel {
+  final String accessToken;
+
+  RemoteAccountModel(this.accessToken);
+
+  factory RemoteAccountModel.fromJson(Map json) =>
+      RemoteAccountModel(json['accessToken']);
+
+  AccountEntity toEntity() =>
+      AccountEntity(accessToken);
+}
