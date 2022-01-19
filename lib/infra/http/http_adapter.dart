ea18a3470d0ff9c4419b7ceba89d1411d715d550
Nicolas Teixeira <nicolasteixeira3856@outlook.com>
2022-01-18 21:21:13 -0300

    ensure HttpAdapter throws ServerError if post throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -10,15 +10,20 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request({@required String url, @required String method, Map body}) async {
+  Future<Map> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
-    if (method == 'post') {
-      response = await client.post(url, headers: headers, body: jsonBody);
+    try {
+      if (method == 'post') {
+        response = await client.post(url, headers: headers, body: jsonBody);
+      }
+    } catch (error) {
+      throw HttpError.serverError;
     }
     return _handleResponse(response);
   }
